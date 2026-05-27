0c2bb02971b76ce04b242c0e2d7475fa6c010c12
J-Luiz2 <Joaquimluizrego@gmail.com>
2026-05-27 17:34:27 -0300

    adiciona a função listaraparelhos
    adiciona a função de listar aparelhos que adiciona todas as informações do aparelho em uma lista no html, com a funcionalidade de remover cada item individualmente

--- js/script1.js
+++ js/script1.js
@@ -4,7 +4,9 @@ const botao = document.getElementById("btnConsumo");
 const botaomes = document.getElementById("botaomes");
 const resultadoWh = document.getElementById("resultado");
 const resultadoKWH = document.getElementById("resultadoKWH");
-
+const listaAparelhos = document.getElementById("listaAparelhos");
+const nomeAparelho = document.getElementById("nome");
+const botaoAdicionar = document.getElementById("btnlista");
 
 function consumodiario() {
   const watts = Number(inputwatts.value);
@@ -29,7 +31,42 @@ function consumomensal() {
   resultadoKWH.textContent = "Consumo mensal: " + mensalKWH + " kWh";
 }
 
+function listaraparelhos() {
+  const nome = nomeAparelho.value.trim();
+  const watts = Number(inputwatts.value);
+  const horas = Number(inputhoras.value);
+
+  const wh = watts * horas;
+  const kwh = wh / 1000;
+
+  const mensalWh = wh * 30;
+  const mensalKWH = mensalWh / 1000;
+
+  if (nome === '' || watts === "" || horas === "") {
+    return
+  }
+
+  const listItem = document.createElement("li");
+  listItem.textContent = `${nome} - Consumo diário: ${wh} Wh (${kwh} kWh), Consumo mensal: ${mensalWh} Wh (${mensalKWH} kWh)`;
+  listaAparelhos.appendChild(listItem);
+
+  const btnRemover = document.createElement('button');
+  btnRemover.textContent = 'X'
+  btnRemover.className = 'btn-remover';
+              
+  btnRemover.onclick = function() { 
+    listItem.remove(); 
+  };
+
+  listItem.appendChild(btnRemover);
+
+  nomeAparelho.value = '';
+  inputwatts.value = '';
+  inputhoras.value = '';
+}
 
 botao.addEventListener("click", consumodiario);
 
 botaomes.addEventListener("click", consumomensal);
+
+botaoAdicionar.addEventListener("click", listaraparelhos);

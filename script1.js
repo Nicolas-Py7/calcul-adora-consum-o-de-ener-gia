237e46ff138bcb749d80dec29d6c827b40ea2341
Nicolas <nicolas.p@escolar.ifrn.edu.br>
2026-05-28 15:50:30 -0300

    mudança nos arquivos de códigos

---
+++ script1.js
@@ -0,0 +1,93 @@
+const inputwatts = document.getElementById("watts");
+const inputhoras = document.getElementById("horas");
+const botao = document.getElementById("btnConsumo");
+const botaomes = document.getElementById("botaomes");
+const resultadoWh = document.getElementById("resultado");
+const resultadoKWH = document.getElementById("resultadoKWH");
+const listaAparelhos = document.getElementById("listaAparelhos");
+const nomeAparelho = document.getElementById("nome");
+const botaoAdicionar = document.getElementById("btnlista");
+const botaoVisualizar = document.getElementById("bntmostrarlista")
+const listaSection = document.getElementById("sectionlista")
+let aparelhos = [];
+
+function consumodiario() {
+  const watts = Number(inputwatts.value);
+  const horas = Number(inputhoras.value);
+
+  const wh = watts * horas;
+  const kwh = wh / 1000;
+
+  resultadoWh.textContent = "Consumo diário: " + wh + " Wh";
+  resultadoKWH.textContent = "Consumo diário: " + kwh + " kWh";
+}
+
+function consumomensal() {
+  const watts = Number(inputwatts.value);
+  const horas = Number(inputhoras.value);
+
+  const mensalWh = watts * horas * 30;
+  const mensalKWH = mensalWh / 1000;
+
+  resultadoWh.textContent = "Consumo mensal: " + mensalWh + " Wh";
+  resultadoKWH.textContent = "Consumo mensal: " + mensalKWH + " kWh";
+}
+
+function salvaraparelho() {
+  const nome = nomeAparelho.value;
+  const watts = Number(inputwatts.value);
+  const horas = Number(inputhoras.value);
+  
+  const wh = watts * horas;
+  const kwh = wh / 1000;
+
+  const mensalWh = wh * 30;
+  const mensalKWH = mensalWh / 1000;
+
+  const aparelho = {
+    nome: nome,
+    potencia: watts,
+    tempo_de_uso: horas,
+    consumodiario: kwh,
+    consumomensal: mensalKWH
+  }
+
+  aparelhos.push(aparelho);
+}
+
+function renderizarLista() {
+
+    listaAparelhos.innerHTML = "";
+
+    aparelhos.forEach((aparelho, index) => {
+
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            ${aparelho.nome} -
+            ${aparelho.consumodiario} kWh/dia -
+            ${aparelho.consumomensal} kWh/mês
+        `;
+
+        listaAparelhos.appendChild(li);
+
+    });
+
+}
+
+function visualizarLista() {
+  if (listaSection.style.display === "none" || listaSection.style.display === "") {
+    listaSection.style.display = "flex";
+  } 
+
+  else {
+    listaSection.style.display = "none";
+  }
+}
+
+botao.addEventListener("click", consumodiario);
+
+botaomes.addEventListener("click", consumomensal);
+
+botaoAdicionar.addEventListener("click", salvaraparelho);
+
